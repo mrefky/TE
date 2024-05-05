ded85f77807052c43fef0a8868538ad9b69b0ca5
mrefky <mrefky@hotmail.com>
2024-05-05 09:27:03 +0300

    1

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ microk8s kubectl delete sts zookeeper
 microk8s kubectl delete sts --all
 microk8s kubectl delete deploy --all
 microk8s kubectl delete svc --all
-microk8s kubectl delete pvc --all
+microk8s kubectl delete po,pvc --all
 
 sleep 1
 cd ~/TE/project/mysql
