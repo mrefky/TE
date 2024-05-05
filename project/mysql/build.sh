7cf2ae1be97de0b2432cd8400f05ac8d3b6d2113
mrefky <mrefky@hotmail.com>
2024-05-05 08:17:42 +0300

    1

--- project/mysql/build.sh
+++ project/mysql/build.sh
@@ -2,15 +2,21 @@ echo "-------------------------------------"
 echo "---   Delete mysql ------------------"
 echo "-------------------------------------" 
 microk8s kubectl delete -f .
+echo "-------------------------------------"
+echo "---   Create mysql statefullset -----"
+echo "-------------------------------------"
 microk8s kubectl create -f .
 sleep 10
 echo "-------------------------------------"
-echo "---   Create mysql statefullset -----"
+echo "---   Wait till all are up  ---------"
 echo "-------------------------------------" 
 
 microk8s kubectl wait pods mysql-0  --for condition=Ready --timeout=90s
 microk8s kubectl wait pods mysql-1  --for condition=Ready --timeout=90s
 microk8s kubectl wait pods mysql-2  --for condition=Ready --timeout=90s
+echo "-------------------------------------"
+echo "---   Expose all  -------------------"
+echo "-------------------------------------"
 ./expose.sh
 microk8s kubectl delete po mysql-client
 echo "-------------------------------------"
